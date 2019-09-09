9456f2e461017ed797bd9334efe20e4d48b3582f
AvDa-koder <dasabhi725@gmail.com>
2019-09-10 00:18:52 +0600

    initial commit

---
+++ assets/js/active.js
@@ -0,0 +1,16 @@
+(function ($) {
+	"use strict";
+	jQuery(document).ready(function($){
+		$(".homepage-slides").owlcarousel({
+			items: 1,
+			nav: true,
+			dots: false,
+			autoplay: false,
+			loop: true,
+		})
+	});
+	jQuery(window).load(function () {
+
+	});
+
+}(jQuery));
